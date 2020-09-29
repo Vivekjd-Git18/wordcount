de46745f0e1812b3d5a9c37fe4c9d0c2d2b86788
vivek <vivekjd90@gmail.com>
2020-09-29 16:56:23 +0530

    first commit

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
 	path('count/', views.count, name='count'),
 	path('', views.homepage, name='home'),
-	path('', views.About, name='About') 
+	path('', views.About, name='About us') 
     
 	
 
